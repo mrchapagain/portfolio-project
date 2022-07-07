d723aef80e61e0ea5074cc2d532b6c548740a67c
Mukti R. Chapagain <mrchapagain@gmail.com>
2022-07-07 21:47:45 +0200

    job is updated and working

--- job/views.py
+++ job/views.py
@@ -4,9 +4,9 @@ from .models import Job
 # Create your views here.
 def  home(request):
     jobs= Job.objects
-    return render(request, 'job/home.html', {'jobs': jobs})
+    return render(request, 'job/home.html', {'job': jobs})
 
 def  alljobs(request):
     jobs= Job.objects
-    return render(request, 'job/alljobs.html', {'jobs': jobs})
+    return render(request, 'job/alljobs.html', {'job': jobs})
 
