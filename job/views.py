66b3e5cf10d245df4c0178d868469e6cedea7597
Mukti R. Chapagain <mrchapagain@gmail.com>
2022-07-15 23:25:19 +0200

    template extenction of job app is implemented

--- job/views.py
+++ job/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Job
 from blog.models import Blog
 #from plotly.offline import plot
+# in the template {% outoscape off%} & {% endautoscape %}
 
 # Create your views here.
 def  home(request):
