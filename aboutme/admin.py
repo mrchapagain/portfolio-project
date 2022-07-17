d88988cd8e769c318093daf2aaec631e8be53aa5
Mukti R. Chapagain <mrchapagain@gmail.com>
2022-07-17 16:08:06 +0200

    base-extention and Tab button is fixed

--- aboutme/admin.py
+++ aboutme/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import AboutMe
+from .models import AboutMe, MyCv
 
 admin.site.register(AboutMe)
+admin.site.register(MyCv)
--- aboutme/urls.py
+++ aboutme/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('', views.alldocs, name='alldocs'),
     path('<int:aboutme_id>/', views.docdetail, name='docdetail'),
+
 ]
