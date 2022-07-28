7c47bef23d215593453d3c9abb431c192a2f90c8
Mukti R. Chapagain <mrchapagain@gmail.com>
2022-07-28 21:42:23 +0200

    food data blogis adjusted

--- blog/models.py
+++ blog/models.py
@@ -12,7 +12,7 @@ class Blog(models.Model):
         return self.title
     
     def summary(self):
-        return self.sum[:500]
+        return self.sum[:255]
 
     def pub_date_pretty(self):
         return self.pub_date.strftime('%b %e %Y')
