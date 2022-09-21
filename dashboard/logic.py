5d131c0e37b6b073aee45aac83d8ce6947a15079
Mukti R. Chapagain <mrchapagain@gmail.com>
2022-09-22 01:18:36 +0200

    figure are in place.just need to adjust info

--- dashboard/logic.py
+++ dashboard/logic.py
@@ -14,6 +14,7 @@ from matplotlib import pyplot as plt
 import warnings
 warnings.filterwarnings("ignore")
 
+
 class FridaDataAnalytics():
 
   def frida_datareader(self, frida_data_object):
@@ -53,3 +54,94 @@ class FridaDataAnalytics():
     df_food_name= df_frida[df_frida.FødevareNavn == food_name]
 
     return df_food_name
+
+
+  def piechart_fooditem_energy(self, df_food_name):
+        self.df_food_name=df_food_name
+
+        # Pie chart, where the slices will be ordered and plotted counter-clockwise:
+        labels = ["Fedt_g", "Kulhydrat_g", "Protein_g"]
+        sizes =  df_food_name.loc[:, ["Protein_deklaration_g", 'Kulhydrat_deklaration_g', 'Fedt_total_g']].values.tolist()[0]# 3rd to 8th column value from indivisual-item 
+        #sizes= [i*0 for i in sizes if i < 0 ]
+        explode = (0, 0, 0.05)  # only "explode" the 3rd slice (i.e. 'Protein')
+
+        fig, ax = plt.subplots()
+        ax.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%', shadow=True, startangle=90)
+        ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+        #plt.title(f'"Total Energy in KJ: {df_food_name.Energy_KJ.values}"', fontsize=12)
+        ax.text(1.30, 0.005, f'"Total Energy: {df_food_name.Energy_kj.item()} kj / {df_food_name.Energy_kcal.item()} kcal"', fontsize=12, verticalalignment='bottom', horizontalalignment='right', transform=ax.transAxes, color='red')
+
+        plt.tight_layout()
+        graph=get_graph()
+        return graph
+
+
+  def piechart_fooditem_co2(self, df_fooditem_climate):
+        self.df_fooditem_climate=df_fooditem_climate
+        
+        # Pie chart, where the slices will be ordered and plotted counter-clockwise:
+        labels = ['Agriculture', 'iLUC', 'Processing', 'Packaging', 'Transport', 'Retail']
+        sizes =  df_fooditem_climate[labels].values.tolist()[0] # 3rd to 8th column value from indivisual-item 
+        sizes = [0 if i < 0 else i for i in sizes] # Negetive value replace with 0
+
+        explode = (0, 0, 0, 0, 0.1, 0)  # only "explode" the 5th slice (i.e. 'Transport')
+
+        fig, ax = plt.subplots()
+        ax.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%', shadow=True, startangle=90)
+        ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+        #plt.title(f'"Total CO2 contribution equivalent per KG: {df_fooditem_climate.Total_CO2_eq_perkg.values}"', fontsize=12)
+        ax.text(1.30, 0.005, f'"Total CO2 contribution equivalent per KG in grams: {df_fooditem_climate.Total_CO2_eq_perkg.item()}"', fontsize=12, verticalalignment='bottom', horizontalalignment='right', transform=ax.transAxes, color='red')
+        plt.tight_layout()
+        graph=get_graph()
+        #ax.legend()
+        return graph
+
+
+  def co2_data_plot(self, selected_dropped_data):
+        self.selected_dropped_data= selected_dropped_data
+        # Group some of the elements from columns with categorical data
+        df_with_category= selected_dropped_data[['Category_en', 'Agriculture', 'iLUC', 'Processing', 'Packaging',	'Transport',	'Retail', 'Total_CO2_eq_perkg']].groupby(by= ['Category_en'], sort=True).mean().sort_values(by=['Total_CO2_eq_perkg'], ascending=False).round(decimals = 2)
+        
+        # Stacked bar plot
+        rows= selected_dropped_data.shape[0]
+        plt.switch_backend('AGG')
+        fig, ax= plt.subplots(figsize=(10, 5))
+        ax.barh(df_with_category.index, df_with_category['Agriculture'], label= "Agriculture")
+        ax.barh(df_with_category.index, df_with_category['iLUC'], label='iLUC')
+        ax.barh(df_with_category.index, df_with_category['Packaging'], label='Processing')
+        ax.barh(df_with_category.index, df_with_category['Packaging'], label='Packaging')
+        ax.barh(df_with_category.index, df_with_category['Transport'], label='Transport')
+        ax.barh(df_with_category.index, df_with_category['Retail'], label='Retail')
+
+        #sns.barplot(df2.values, df2.index, alpha=1)
+        plt.xticks(rotation=20)
+        plt.title(f'"Share of the Total CO2 contribution based on food category"')
+        plt.ylabel("Product Category")
+        plt.xlabel("Total average CO2 equivalent/Kg")
+        ax.legend()
+        plt.tight_layout()
+        graph=get_graph()
+        return graph  
+
+
+
+  def comparision_barplot_climate(self, df_climate, df_fooditem_climate, foodname):
+      self.df_climate=df_climate
+      self.df_fooditem_climate= df_fooditem_climate
+      self.foodname= foodname
+      plt.switch_backend('AGG')
+
+      plt.figure(figsize=(4,3))
+      #plt.title(title, fontsize=8)
+      #sns.bar(df_fooditem_climate.Analysis.unique(), df_fooditem_climate['Analysis'].value_counts(), color ='grey', width = 0.4)
+
+      sns.barplot(data= df_fooditem_climate, x= ["Vegetables_Average","Meat/Poultry_Average",{foodname}], y= [1,2,3], palette= {"Vegetables_Average": "blue", "Meat/Poultry_Average": "red", {foodname}: "green"})
+
+      #df_fooditem_climate['Analysis'].value_counts().plot(kind='bar', color= {"Neutral": "blue", "Negetive": "red", "Positive": "green"})
+      plt.xticks(rotation=20)
+      plt.xlabel('Name of Food Category')
+      plt.ylabel('CO2 emission contribution')
+      plt.tight_layout()
+      graph=get_graph()
+      return graph
+
--- dashboard/views.py
+++ dashboard/views.py
@@ -9,32 +9,48 @@ def Food_category(request):
     cls= FridaDataAnalytics()
     df= cls.frida_datareader(data_frida_final)
     df_category= cls.df_FødevareGruppe(df, category)
-    name_list=df_category.FødevareNavn.to_list()
+    food_group_list= cls.FødevareGruppe_list(df)
 
-    return render(request, 'dashboard/alldashboards.html', {"category":category, "df":df, "df_category":df_category, "name_list":name_list})
+    name_list=df_category.FødevareNavn.to_list()
+    
+    return render(request, 'dashboard/alldashboards.html', {"category":category, "df_category":df_category, "name_list":name_list, "food_group_list":food_group_list})
 
 
 def foodname_todf(request):
     foodname= request.POST['food_name']
 
     data_frida_final= "https://github.com/mrchapagain/ConsumerDataAnalytics/raw/main/finaldf_fridanutrient_aditionallink_groups.xlsx"
+    data_climate_selected= "https://github.com/mrchapagain/ConsumerDataAnalytics/raw/main/df_climate_selected.xlsx"
+    
     cls= FridaDataAnalytics()
     df= cls.frida_datareader(data_frida_final)
+    df_climate= cls.frida_datareader(data_climate_selected)
+    
+    food_group_list= cls.FødevareGruppe_list(df)
     df_food_name= cls.df_FødevareNavn(df, foodname)
 
-    return render(request, 'dashboard/alldashboards.html', {"foodname":foodname, "df_food_name":df_food_name})
+    cat=df_food_name.FødevareGruppe.item()
+    name_list=df[df.FødevareGruppe == cat].FødevareNavn.to_list()
+
+    pie_chart_energy= cls.piechart_fooditem_energy(df_food_name)
+
+    df_foodname_climate= df_climate[df_climate.Product_dk== foodname]
+    pie_chart_climate= cls.piechart_fooditem_co2(df_foodname_climate)
+
+    group_plot_climate= cls.co2_data_plot(df_climate)
+    barplot_climate_comparision=cls.comparision_barplot_climate(df_climate, df_foodname_climate,foodname)
+
+    return render(request, 'dashboard/alldashboards.html', {"foodname":foodname, "df_food_name":df_food_name, "food_group_list":food_group_list, "name_list":name_list, "pie_chart_energy":pie_chart_energy, "pie_chart_climate":pie_chart_climate, "group_plot_climate":group_plot_climate, "barplot_climate_comparision":barplot_climate_comparision})
 
 
 def item_todisplay(request):
     data_frida_final= "https://github.com/mrchapagain/ConsumerDataAnalytics/raw/main/finaldf_fridanutrient_aditionallink_groups.xlsx"
     
-    #category_chosen= request.POST['food_cat']
-    
     cls= FridaDataAnalytics()
     df= cls.frida_datareader(data_frida_final)
-    df_name_list= cls.FødevareNavn_list(df)
-    df_group_list= cls.FødevareGruppe_list(df)
+    food_name_list= cls.FødevareNavn_list(df)
+    food_group_list= cls.FødevareGruppe_list(df)
     df_group_name_dict= cls.FødevareGruppe_FødevareNavn_dict(df)
 
-    return render(request, 'dashboard/alldashboards.html', {'df': df, "df_name_list":df_name_list, "df_group_list":df_group_list, "df_group_name_dict":df_group_name_dict})
+    return render(request, 'dashboard/alldashboards.html', {"food_name_list":food_name_list, "food_group_list":food_group_list, "df_group_name_dict":df_group_name_dict})
     
