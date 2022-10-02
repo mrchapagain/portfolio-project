9eda870416014add4bc72d0a787ac3b4c1ea12ed
Mukti R. Chapagain <mrchapagain@gmail.com>
2022-10-03 01:49:30 +0200

    blogincluding detail html page is adjusted

--- blog/views.py
+++ blog/views.py
@@ -5,10 +5,13 @@ from .models import Blog
 # Create your views here.
 
 def allblogs(request):
-    blogs= Blog.objects
-    return render(request, 'blog/allblogs.html', {'blogs': blogs})
+    blogs= Blog.objects.all()
+    #detailblog = get_object_or_404(Blog, pk=blog_id)
+    sample_blog= [blog for blog in blogs if blog.title == "blog check"]
+    return render(request, 'blog/allblogs.html', {'blogs': blogs, "sample_blog":sample_blog})
 
 def detail(request, blog_id):
+    blogs= Blog.objects
     detailblog = get_object_or_404(Blog, pk=blog_id)
-    return render(request, 'blog/detail.html', {'blog': detailblog})
+    return render(request, 'blog/detail.html', {'blogs': blogs, 'blog': detailblog})
 
