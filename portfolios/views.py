14b06badd6a4e7c8e4b2bb9a3dc0821f2c176f30
Mukti R. Chapagain <mrchapagain@gmail.com>
2022-10-03 10:36:52 +0200

    portfoliodisplay adjusted

--- portfolios/views.py
+++ portfolios/views.py
@@ -16,14 +16,16 @@ def  home(request):
     return render(request, 'portfolios/home.html', {'portfolioss': portfolioss, 'blogs': blogs, 'googles':googles, 'tweets':tweets, 'climates':climates, 'healths':healths, 'ontologys':ontologys})
 
 def  allportfolioss(request):
-    portfolioss= Portfolios.objects
+    portfolioss= Portfolios.objects.all()
     #googles= Google.objects
     #tweets=Tweet.objects
     #climates=Climate.objects
     #healths=Health.objects
     #ontologys=Ontology.objects
-    return render(request, 'portfolios/allportfolioss.html', {'portfolioss': portfolioss})
+    sample_portfolios= [portfolios for portfolios in portfolioss if portfolios.title == "Portfolios sample"]
+    return render(request, 'portfolios/allportfolioss.html', {'portfolioss': portfolioss, "sample_portfolios":sample_portfolios})
 
 def portfoliosdetail(request, portfolios_id):
+    portfolioss= Portfolios.objects
     detailportfolios = get_object_or_404(Portfolios, pk=portfolios_id)
-    return render(request, 'portfolios/portfoliosdetail.html', {'portfolios': detailportfolios})
+    return render(request, 'portfolios/portfoliosdetail.html', {"portfolioss":portfolioss, 'portfolios': detailportfolios})
